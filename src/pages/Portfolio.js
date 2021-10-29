9893ab005e7587969db21d5ef6044d9dd3b90814
gail enstice <skyedoll@aol.com>
2021-10-28 21:50:32 -0400

    portfolio template and bootstrap columns

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Container, CardColumns } from 'react-bootstrap';
+import Project from '../components/Project';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+// Array of projects is imported, keeping this file focused on the page
+import projects from '../assets/projects';
+
+// Using react-bootstrap's CardColumns component greatly simplifies the code for this page, replacing Rows/Cols I defined in original portfolio
+// Each item component is a card that takes a project's info as props,
+export default function Portfolio() {
+  return (
+    <Container className="main">
+      <Header page={'Portfolio'} />
+      <CardColumns className="pb-5 pt-5">
+        {projects.map((project) => (
+          <Project
+            name={project.name}
+            src={project.src}
+            git={project.git}
+            live={project.live}
+          />
+        ))}
+      </CardColumns>
+      <Footer />
+    </Container>
+  );
+}
