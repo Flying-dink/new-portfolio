dbc52218940eb1d0a855ecc692c708711bfd0446
gail enstice <skyedoll@aol.com>
2021-10-28 23:35:01 -0400

    imported pictures

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,8 +1,8 @@
 // List of projects for portfolio, stored here to reduce imports/clutter on portfolio page
 // Import images for project cards, numbered for easier replacement
-import img1 from './  ';
+import img1 from './ 2021-07-29(2).png ';
 import img2 from './ 2021-09-22 (3).png ';
-import img3 from './  ';
+import img3 from './ 2021-08-29(2).png ';
 import img4 from './  ';
 import img5 from './  ';
 import img6 from './  ';
