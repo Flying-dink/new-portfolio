516846fb6aac43a40e83c8237a73a932f3951345
gail enstice <skyedoll@aol.com>
2021-10-31 19:43:23 -0400

    fixed pictures

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -30,6 +30,41 @@ const Projects = () => {
                 </a>
                 <h3 className="projectText">This application allows users to create notes that will be saved to a json file instead of a database. It also allows user to delete previous notes and will update the json file with the new list of notes after each note is deleted.</h3>
             </div>
+        
+            <div className="project">
+            <div className="projectDiv">
+                <h1 className="projectTitle">Read My Lips</h1>
+                <h2 className="projectTitle"><a href="https://github.com/Flying-dink/Read-My-Lips" className="linkStyles" target="_blank">Github</a></h2>
+                <a href="https://flying-dink.github.io/Read-My-Lips/" target="_blank">
+                    <img src={readmylipsIMG} className="projectImage" alt="Read My lips" />
+                </a>
+                <h3 className="projectText">Read my lips is a game wher one can read another's lips by guessing</h3>
+            </div>
+            <div className="projectDiv">
+                <h1 className="projectTitle">MyAdventure App</h1>
+                <h2 className="projectTitle"><a href="https://github.com/NukaGrizz/MyAdventure" className="linkStyles" target="_blank">Github</a></h2>
+                <a href="https://myadventureteam.herokuapp.com/" target="_blank">
+                    <img src={MyadventureAppIMG} className="projectImage" alt="My adventure App" />
+                </a>
+                <h3 className="projectText">This application is a social media site where one can post about adventures.</h3>
+            </div>
+            <div className="projectDiv">
+                <h1 className="projectTitle">Note Taker</h1>
+                <h2 className="projectTitle"><a href="https://github.com/Flying-dink/Note-Taker" className="linkStyles" target="_blank">Github</a></h2>
+                <a href="https://note-taker-app-2021.herokuapp.com/" target="_blank">
+                    <img src={noteTakerIMG} className="projectImage" alt="note taker" />
+                </a>
+                <h3 className="projectText">This application allows users to create notes that will be saved to a json file instead of a database. It also allows user to delete previous notes and will update the json file with the new list of notes after each note is deleted.</h3>
+            </div>
+        
+                  
+
+        
+
+            
+
+        </div>
+
             
             </div>
         
